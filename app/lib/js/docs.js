48a5596f024dbe8e11a086385202e09ef75813e1
Tyler Benton <tjbenton21@gmail.com>
2015-05-18 12:53:45 -0400

    Updated to use `fs.existsSync`

--- app/lib/js/docs.js
+++ app/lib/js/docs.js
@@ -180,221 +180,220 @@ var docs = (function(){
   for(var current_file = 0, total_files = files.length; current_file < total_files; current_file++){
    var path = files[current_file],
        filetype = path.slice(path.lastIndexOf(".") + 1),
+       file_exists = fs.existsSync(path),
        setting = _.settings(filetype),
        parsers = _.parsers(filetype),
        parser_keys = Object.keys(parsers),
        output = {};
 
-   fs.exists(path, function(exists){
-    if(!exists){
-     console.error("`" + path + "`" + " doesn't exist");
-    }else{
-     var file = fs.readFileSync(path) + "", // the `""` converts the file from a buffer to a string
+   if(!file_exists){
+    console.error("`" + path + "`" + " doesn't exist");
+   }else{
+    var file = fs.readFileSync(path) + "", // the `""` converts the file from a buffer to a string
 
-         // @description Parses the file and returns the comment blocks in an array
-         // @arg [string] file
-         // @returns [array] of the comment blocks
-         blocks = (function(){
-          var _blocks = [],
-              block_info,
-              lines = file.split(/\n/),
-              in_code = false,
-              in_comment = false;
-
-          // loop over each line in the file and gets the comment blocks
-          for(var i = 0, l = lines.length; i < l; i++){
-           var line = lines[i],
-               comment_index = line.indexOf(setting.block_comment);
-
-           // a) The line is a comment
-           // b) There was a previous comment block
-           if(comment_index > -1){
-            line = line.slice(comment_index + setting.block_comment.length);
-
-            // a) The previous line wasn't a comment
-            if(!in_comment){
-             in_code = false;
-
-             // a) There was block that has already been processed
-             if(!is.undefined(block_info)){
-              block_info.code.end = i - 1;
-              _blocks.push(block_info);
-             }
-
-             // reset the `block_info` to use on the new block
-             block_info = {
-              file: {
-               contents: file,
-               path: path,
-               type: filetype,
-               start: 0,
-               end: file.split("\n").length - 1
-              },
-              comment: {
-               contents: [],
-               start: i,
-               end: 0
-              },
-              code: {
-               contents: [],
-               start: 0,
-               end: 0
-              }
-             };
-
-             in_comment = true;
-            }
-
-            // adds this line to block_info comment contents
-            block_info.comment.contents.push(line);
-
-            // a) The last line in the file is a commment
-            if(i === l - 1){
-             block_info.comment.end = i;
-             _blocks.push(block_info);
-            }
-           }else if(!is.undefined(block_info)){
-            if(in_comment){
+        // @description Parses the file and returns the comment blocks in an array
+        // @arg [string] file
+        // @returns [array] of the comment blocks
+        blocks = (function(){
+         var _blocks = [],
+             block_info,
+             lines = file.split(/\n/),
+             in_code = false,
              in_comment = false;
-             block_info.comment.end = i - 1; // -1 because the end was the line before
-            }
 
-            // a) The previous line was a comment
-            if(!in_code){
-             in_code = true;
-             block_info.code.start = i;
-            }
+         // loop over each line in the file and gets the comment blocks
+         for(var i = 0, l = lines.length; i < l; i++){
+          var line = lines[i],
+              comment_index = line.indexOf(setting.block_comment);
 
-            // adds this line to block code contents
-            block_info.code.contents.push(line);
+          // a) The line is a comment
+          // b) There was a previous comment block
+          if(comment_index > -1){
+           line = line.slice(comment_index + setting.block_comment.length);
 
-            // a) pushes the last block onto the _blocks
-            if(i === l - 1){
-             block_info.code.end = i;
+           // a) The previous line wasn't a comment
+           if(!in_comment){
+            in_code = false;
+
+            // a) There was block that has already been processed
+            if(!is.undefined(block_info)){
+             block_info.code.end = i - 1;
              _blocks.push(block_info);
             }
-           }
-          } // end loop
 
-          return _blocks;
-         })(),
-
-         // @description Parses each block in blocks
-         // @arg [array] the blocks that are returned from blocks
-         // @returns [array]
-         parsed_blocks = (function(blocks){
-          var _parsed_blocks = [];
-
-          // loop over each block
-          for(var a = 0, blocks_length = blocks.length; a < blocks_length; a++){
-           var block = blocks[a],
-               to_parse = block.comment.contents,
-               _parsers_in_block = {},
-               _current_parser_info = {};
-
-           block.comment.contents = _.normalize(block.comment.contents);
-           block.code.contents = _.normalize(block.code.contents);
-
-           // loop over each line in the comment block
-           for(var i = 0, l = to_parse.length; i < l; i++){
-            var line = to_parse[i],
-                parser_prefix_index = line.indexOf(setting.parser_prefix);
-
-            // a) there is an index of the parser prefix
-            if(parser_prefix_index >= 0){
-             var first_space = line.indexOf(" ", parser_prefix_index),
-                 name_of_parser = line.slice(parser_prefix_index + 1, first_space >= 0 ? first_space : line.length);
-
-             // a) the name is one of the parser names
-             if(parser_keys.indexOf(name_of_parser) >= 0){
-              if(!is.empty(_current_parser_info)){
-               _current_parser_info.end = i - 1;
-               parse(_current_parser_info, block);
-              }
-
-              // redefines resets the current parser to be blank
-              _current_parser_info = {
-               name: name_of_parser, // sets the current parser name
-               line: line.slice(parser_prefix_index + 1 + name_of_parser.length).trim(), // removes the current parser name and it's prefix from the first line
-               contents: [],
-               start: i, // sets the starting line of the parser
-               end: 0
-              };
+            // reset the `block_info` to use on the new block
+            block_info = {
+             file: {
+              contents: file,
+              path: path,
+              type: filetype,
+              start: 0,
+              end: file.split("\n").length - 1
+             },
+             comment: {
+              contents: [],
+              start: i,
+              end: 0
+             },
+             code: {
+              contents: [],
+              start: 0,
+              end: 0
              }
-            }
+            };
 
-            if(!is.empty(_current_parser_info)){
-             // adds the current line to the contents
-             _current_parser_info.contents.push(line);
-            }
-
-            // a) is the last line in the comment block
-            if(i === l - 1){
-             _current_parser_info.end = i;
-             _parsed_blocks.push(parse(_current_parser_info, block));
-            }
-           } // end block loop
-          } // end blocks loop
-
-          // @description Used as a helper function because this action is performed in two spots
-          // @arg [string] name of the parser to run
-          // @arg [object] information of the current parser block
-          // @arg [object] information about the current comment block, the code after the comment block and the full file contents
-          function parse(parser_block, block_info){
-           var name = parser_block.name;
-
-           // removes the first line because it's the "line" of the parser
-           parser_block.contents.shift();
-
-           // normalizes the current parser block contents
-           parser_block.contents = _.normalize(parser_block.contents);
-
-           // sets the parser block information to be in it's own namespace of `parser`
-           parser_block = {
-            parser: parser_block
-           };
-
-           // call the parser function and store the result
-           var to_extend = parsers[name].call(_.extend(parser_block, block_info));
-
-           // a) the current item being merged is already defined in the base
-           // b) define the target
-           if(!is.undefined(_parsers_in_block[name])){
-            // a) convert the target to an array
-            // b) add item to the current target array
-            if(!is.array(_parsers_in_block[name])){
-             _parsers_in_block[name] = [_parsers_in_block[name], to_extend];
-            }else{
-             _parsers_in_block[name].push(to_extend);
-            }
-           }else{
-            _parsers_in_block[name] = to_extend;
+            in_comment = true;
            }
-           return _parsers_in_block;
+
+           // adds this line to block_info comment contents
+           block_info.comment.contents.push(line);
+
+           // a) The last line in the file is a commment
+           if(i === l - 1){
+            block_info.comment.end = i;
+            _blocks.push(block_info);
+           }
+          }else if(!is.undefined(block_info)){
+           if(in_comment){
+            in_comment = false;
+            block_info.comment.end = i - 1; // -1 because the end was the line before
+           }
+
+           // a) The previous line was a comment
+           if(!in_code){
+            in_code = true;
+            block_info.code.start = i;
+           }
+
+           // adds this line to block code contents
+           block_info.code.contents.push(line);
+
+           // a) pushes the last block onto the _blocks
+           if(i === l - 1){
+            block_info.code.end = i;
+            _blocks.push(block_info);
+           }
+          }
+         } // end loop
+
+         return _blocks;
+        })(),
+
+        // @description Parses each block in blocks
+        // @arg [array] the blocks that are returned from blocks
+        // @returns [array]
+        parsed_blocks = (function(blocks){
+         var _parsed_blocks = [];
+
+         // loop over each block
+         for(var a = 0, blocks_length = blocks.length; a < blocks_length; a++){
+          var block = blocks[a],
+              to_parse = block.comment.contents,
+              _parsers_in_block = {},
+              _current_parser_info = {};
+
+          block.comment.contents = _.normalize(block.comment.contents);
+          block.code.contents = _.normalize(block.code.contents);
+
+          // loop over each line in the comment block
+          for(var i = 0, l = to_parse.length; i < l; i++){
+           var line = to_parse[i],
+               parser_prefix_index = line.indexOf(setting.parser_prefix);
+
+           // a) there is an index of the parser prefix
+           if(parser_prefix_index >= 0){
+            var first_space = line.indexOf(" ", parser_prefix_index),
+                name_of_parser = line.slice(parser_prefix_index + 1, first_space >= 0 ? first_space : line.length);
+
+            // a) the name is one of the parser names
+            if(parser_keys.indexOf(name_of_parser) >= 0){
+             if(!is.empty(_current_parser_info)){
+              _current_parser_info.end = i - 1;
+              parse(_current_parser_info, block);
+             }
+
+             // redefines resets the current parser to be blank
+             _current_parser_info = {
+              name: name_of_parser, // sets the current parser name
+              line: line.slice(parser_prefix_index + 1 + name_of_parser.length).trim(), // removes the current parser name and it's prefix from the first line
+              contents: [],
+              start: i, // sets the starting line of the parser
+              end: 0
+             };
+            }
+           }
+
+           if(!is.empty(_current_parser_info)){
+            // adds the current line to the contents
+            _current_parser_info.contents.push(line);
+           }
+
+           // a) is the last line in the comment block
+           if(i === l - 1){
+            _current_parser_info.end = i;
+            _parsed_blocks.push(parse(_current_parser_info, block));
+           }
+          } // end block loop
+         } // end blocks loop
+
+         // @description Used as a helper function because this action is performed in two spots
+         // @arg [string] name of the parser to run
+         // @arg [object] information of the current parser block
+         // @arg [object] information about the current comment block, the code after the comment block and the full file contents
+         function parse(parser_block, block_info){
+          var name = parser_block.name;
+
+          // removes the first line because it's the "line" of the parser
+          parser_block.contents.shift();
+
+          // normalizes the current parser block contents
+          parser_block.contents = _.normalize(parser_block.contents);
+
+          // sets the parser block information to be in it's own namespace of `parser`
+          parser_block = {
+           parser: parser_block
           };
 
-          return _parsed_blocks;
-         })(blocks);
+          // call the parser function and store the result
+          var to_extend = parsers[name].call(_.extend(parser_block, block_info));
 
-     // a) the current item being merged is already defined in the base
-     // b) define the target
-     if(!is.undefined(json[filetype])){
-      // a) convert the target to an array
-      // b) add item to the current target array
-      if(!is.array(json[filetype])){
-       json[filetype] = [json[filetype], parsed_blocks];
-      }else{
-       json[filetype].push(parsed_blocks);
-      }
+          // a) the current item being merged is already defined in the base
+          // b) define the target
+          if(!is.undefined(_parsers_in_block[name])){
+           // a) convert the target to an array
+           // b) add item to the current target array
+           if(!is.array(_parsers_in_block[name])){
+            _parsers_in_block[name] = [_parsers_in_block[name], to_extend];
+           }else{
+            _parsers_in_block[name].push(to_extend);
+           }
+          }else{
+           _parsers_in_block[name] = to_extend;
+          }
+          return _parsers_in_block;
+         };
+
+         return _parsed_blocks;
+        })(blocks);
+
+    // a) the current item being merged is already defined in the base
+    // b) define the target
+    if(!is.undefined(json[filetype])){
+     // a) convert the target to an array
+     // b) add item to the current target array
+     if(!is.array(json[filetype])){
+      json[filetype] = [json[filetype], parsed_blocks];
      }else{
-      json[filetype] = parsed_blocks;
+      json[filetype].push(parsed_blocks);
      }
+    }else{
+     json[filetype] = parsed_blocks;
     }
-   });
+   }
   }
 
   // the callback is called after the files have been parsed
-  callback();
+  callback(json);
  };
 
  return _;
