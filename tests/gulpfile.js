18e5df606b4bb18ce2c094b8ed729d0dd0726324
Tyler Benton <tjbenton21@gmail.com>
2015-10-01 13:27:43 -0400

    Updated test files

--- tests/gulpfile.js
+++ tests/gulpfile.js
@@ -1,21 +1,18 @@
 "use strict";
 
 var gulp = require("gulp"),
-    docs = require("docs");
+    docs = require("docs"),
+    path = require("path");
 
 // This is for the documentation
-gulp.task("docs", function(){
- docs
-  .parse("lib/**/*")
-  .then(function(data){
-   console.log("");
-   console.log("");
-   console.log("");
-   console.log("");
-   console.log("-------------------------------------------------------------------");
-   console.log(data);
-  })
-  // .write("test.json");
+gulp.task("docs", function() {
+  docs.parse("lib/**/*")
+    .then(function(result) {
+      console.log(docs.to.json(result.sorted));
+      docs.fs.outputJson("./test.json", result.sorted, {
+        spaces: 2
+      }, 1);
+    });
 });
 
 gulp.task("default", ["docs"]);
