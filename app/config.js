13e14fa447958f83e1489755950bf1f731fc6203
Tyler Benton <tjbenton21@gmail.com>
2015-10-12 11:18:20 -0400

    Updated config to only accept a structure

--- app/config.js
+++ app/config.js
@@ -2,8 +2,9 @@ import {info, fs, is, to, log} from './utils.js'
 
 // changed by `options` key
 const default_options = {
-  files: 'app/**/*', // files to search
-  ignore: ['.*','node_modules/', 'bower_components/', 'jspm_packages/'], // files to be ignored
+  config: `${info.root}/docsfile.js`,
+  files: ['app/**/*'], // files to search
+  ignore: ['.*', 'node_modules/', 'bower_components/', 'jspm_packages/'], // files to be ignored
   changed: true, // determins if only changed files should be parsed or not
   blank_lines: 4, // @todo this stops the current block from adding lines if there're `n` blank line lines between code, and starts a new block.
   annotations: {}
@@ -19,7 +20,7 @@ const default_comment = {
 }
 
 // some defaults for common languages
-let comments = {
+const comments = {
   _: default_comment,
   css: {
     header: { start: '/***', line: '*', end: '***/' },
@@ -39,50 +40,56 @@ let comments = {
   }
 }
 
-export default function config(options = `${info.root}/docs.js`) {
-  let base_config = {
-    ...default_options,
-    comments
-  }
 
-  // try to get the docs file from the passed filepath
-  if (is.string(options)) {
-    try {
-      options = require(options)
-    } catch(err) {
-      options = {}
-    }
-  }
+export const base_config = {
+  ...default_options,
+  comments
+}
 
-  if (is.object(options)) {
-    // ensures `ignore` is always an array
-    if (options.ignore) {
-      options.ignore = to.array(options.ignore)
-    }
+export default function config(options = {}) {
+  let config_file = (options.config ? options : base_config).config
 
+  // try to get the `docsfile.js` so the user config can be merged
+  try {
     // merge the default options with the user options
-    options = to.extend(base_config, ensure_valid_config(options))
-
-    let comments = {};
-    for (let [option, value] of to.entries(options.comments)){
-      // converts option into an array so multiple languages can be declared at the same time
-      option = option.replace(/\s/g, '').split(',');
-
-      for (let lang in option) comments[option[lang]] = value;
-    }
-
-    options.comments = comments;
-
-
-    return options;
-  } else {
-    log.error(`config only accepts path to config file, or '{}' of the options`)
+    config_file = require(config_file)
+  } catch(err) {
+    config_file = {}
   }
+
+  // merge config file with passed options
+  options = to.extend(options, ensure_valid_config(config_file))
+
+  // merge options with base_config so there's a complete list of settings
+  options = to.extend(to.clone(base_config), options)
+
+  // ensures `files`, `ignore` is always an array this way no
+  // more checks have to happen for it
+  options.files = to.array(options.files)
+  options.ignore = to.array(options.ignore)
+
+  // ensures blank_lines is a number to avoid errors
+  options.blank_lines = to.number(options.blank_lines)
+
+  let comments = {}
+
+  // ensures comments are a normal structure (aka not `'rb, py': {...}`)
+  for (let [option, value] of to.entries(options.comments)){
+    // converts option into an array so multiple languages can be declared at the same time
+    option = option.replace(/\s/g, '').split(',')
+
+    for (let lang in option) comments[option[lang]] = value
+  }
+
+  options.comments = comments
+
+
+  return options
 }
 
 
-let valid_options = to.keys(default_options);
-let valid_comment_options = to.keys(default_comment);
+let valid_options = to.keys(default_options)
+let valid_comment_options = to.keys(default_comment)
 
 /// @name ensure_valid_config
 /// @description
@@ -91,7 +98,7 @@ let valid_comment_options = to.keys(default_comment);
 function ensure_valid_config(user_config) {
   for (let key in user_config) {
     if (!is.included(valid_options, key)) {
-      log.warn(`'${key}' is not a valid option, see docs options for more details`); //# @todo add link to the doc options
+      log.warn(`'${key}' is not a valid option, see docs options for more details`) //# @todo add link to the doc options
     }
   }
 
@@ -100,11 +107,11 @@ function ensure_valid_config(user_config) {
     for (let lang in user_config.comments) {
       for (let type in lang) {
         if (!is.included(valid_comment_options, type)) {
-          log.warn(`'${type}' is not a valid comment option in '${lang}', must be 'header', or 'body'`);
+          log.warn(`'${type}' is not a valid comment option in '${lang}', must be 'header', or 'body'`)
         }
       }
     }
   }
 
-  return user_config;
+  return user_config
 }
