635f71973d502e230ef67227d179c7d3b1e674b9
Tyler Benton <tjbenton21@gmail.com>
2015-10-14 12:19:29 -0400

    Added todo comment

--- app/utils/info.js
+++ app/utils/info.js
@@ -8,6 +8,7 @@ info.dir = info.root.split(path.sep) // splits the project dir by the system spe
 info.dir = info.dir[info.dir.length - 1] // gets the working directory
 
 info.temp = {}
+// @todo {8} - come back and change the `.tmp` directory to be inside of `/node_modules/docs/.temp`
 info.temp.folder = path.join(info.root, '.tmp')
 info.temp.file = path.join(info.temp.folder, 'data.json')
 
