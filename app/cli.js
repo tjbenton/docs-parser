19c233d475565e1257a22fee7df2e120df84bafe
Tyler Benton <tjbenton21@gmail.com>
2015-10-31 12:43:03 -0400

    Updated to be `--no-` for debug, warning, change, timestamp

--- app/cli.js
+++ app/cli.js
@@ -7,7 +7,7 @@ import {base_config} from './config'
 export default function cli() {
   // helper functions to parse passed options
   const to_list = (str) => str.replace(/\s/g, '').split(',').filter(Boolean)
-  const to_boolean = (str) => str !== 'false' ? true : false
+  const to_boolean = (str) => false
   const to_number = (str) => ~~str
 
   program
@@ -24,16 +24,30 @@ export default function cli() {
     .option('-f, --files <glob1,[glob2,...]>', `Paths to parsed`, to_list, base_config.files)
     .option('-i, --ignore <glob1,[glob2,...]>', `Paths to ignore`, to_list, base_config.ignore)
     .option('-g, --gitignore', 'Add `gitignore` files to the ignored files', base_config.gitignore)
-    .option('-x, --debug [boolean]', 'Output debugging information', to_boolean, base_config.debug)
-    .option('-w, --warning [boolean]', 'Output warning messages', to_boolean, base_config.warning)
-    .option('-t, --timestamps [boolean]', 'Output timestamps of how long it takes to parse the files', to_boolean, base_config.timestamps)
-    .option('-a, --changed [boolean]', `Parse changed files`, to_boolean, base_config.changed)
+    .option('-x, --no-debug', 'Output debugging information', to_boolean, base_config.debug)
+    .option('-w, --no-warning', 'Output warning messages', to_boolean, base_config.warning)
+    .option('-t, --no-timestamps', 'Output timestamps of how long it takes to parse the files', to_boolean, base_config.timestamps)
+    .option('-a, --no-changed', `Parse changed files`, to_boolean, base_config.changed)
     .option('-b, --blank-lines <n>', `Stops parsing lines after <n> consecutive blank lines`, to_number, base_config.blank_lines)
     .parse(process.argv)
 
+
+  let {
+    noDebug: debug,
+    noWarning: warning,
+    noTimestamps: timestamps,
+    noChanged: changed,
+    blankLines: blank_lines,
+  } = program
+
+
   return docs({
+    debug,
+    warning,
+    timestamps,
+    changed,
     ...program,
-    blank_lines: program.blankLines,
+    blank_lines,
   })
     .then((parsed) => fs.outputJson(program.dest, parsed, { spaces: 2 }))
     .catch((err) => console.error(err.stack))
