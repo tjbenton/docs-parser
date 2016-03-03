5937f07ace52b1f336de5196470662db4352b343
Tyler Benton <tjbenton21@gmail.com>
2016-03-03 12:42:46 -0500

    Added documentation for the block info annotation

--- app/annotations.js
+++ app/annotations.js
@@ -229,11 +229,38 @@ annotations.alias = {
 }
 
 
+
+/// @name blockinfo
+/// @description
+/// This annotation is a special one in that it's only autofilled, and it adds
+/// information about the current block
+///
+/// Here's an example of the information that it returns
+///
+/// ```
+/// "blockinfo": {
+///   "comment": {
+///     "start": 1,
+///     "end": 3,
+///     "type": "header"
+///   },
+///   "code": {
+///     "start": -1,
+///     "end": -1
+///   },
+///   "file": {
+///     "path": "docs/tests/annotations/access/access.header.js",
+///     "start": 1,
+///     "end": 4
+///   }
+/// }
+/// ```
 annotations.blockinfo = {
   autofill() {
-    let comment = this.comment
-    let code = this.code
-    let file = this.file
+    let obj = to.clone(this)
+    let comment = obj.comment
+    let code = obj.code
+    let file = obj.file
     delete comment.contents
     delete code.contents
     delete file.contents
@@ -242,6 +269,10 @@ annotations.blockinfo = {
     delete file.comment
 
     return { comment, code, file }
+  },
+  parse() {
+    this.log.emit('warning', "Passed @blockinfo, it's only supposed to be an autofilled annotation", logAnnotationError(this, ''))
+    return
   }
 }
 
