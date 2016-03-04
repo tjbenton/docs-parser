54932ef19e3275f40cc403a5d3f3074930f80021
Tyler Benton <tjbenton21@gmail.com>
2016-03-03 19:24:47 -0500

    updated @state to be awesome

--- app/annotations.js
+++ app/annotations.js
@@ -508,7 +508,8 @@ annotations.markup = {
       )
       .join('\n')
 
-    const state_interpolation = /\s*\{@states?[^\}]*\}\s*/gi
+    // @todo {10} update this use use the user defined options
+    const state_interpolation = /\s*\${@states?[^\}]*\}\s*/gi
     let raw_stateless = raw.replace(state_interpolation, '')
     let escaped_stateless = escaped.replace(state_interpolation, '')
 
@@ -516,18 +517,21 @@ annotations.markup = {
       settings = to.object(list(settings).map((setting) => setting.split('=')))
     }
 
-    return [
-      {
-        id,
-        language,
-        settings,
-        description: markdown(description),
-        raw,
-        escaped,
-        raw_stateless,
-        escaped_stateless,
-      }
-    ]
+
+    let result = {
+      id,
+      language,
+      settings,
+      description: markdown(description),
+      raw,
+      escaped,
+      raw_stateless,
+      escaped_stateless,
+    }
+
+    Object.defineProperty(result, '__details', { __proto__: null, value: this })
+
+    return [ result ]
   },
   resolve() {
     return this.map((obj, i) => {
@@ -752,44 +756,152 @@ annotations.since = {
   }
 }
 
-/// @name @state
+/// @name @states
+/// @alias @state
 /// @page annotations
 /// @description A state of a the documented item
 /// @returns {hashmap}
+/// @note {10} This annotation must be used with @markup
 /// @markup Usage
-/// /// @states (id) {state} [state_id] - description
+/// /// @state {state}
+/// /// @state {state}
+/// /// @state {state}
+/// /// @state {state}
 ///
-/// /// @states (id) {state} [state_id] - description
-/// /// @states (id) {state} [state_id] - description
-/// /// @states (id) {state} [state_id] - description
+/// /// @state (id) {state} [state_id] - description
+/// /// @state (id) {state} [state_id] - description
+/// /// @state (id) {state} [state_id] - description
+///
+/// /// @state (id)
+/// /// {state} - description
+/// /// {state} - description
+/// /// {state} - description
 ///
 /// /// @states (id)
 /// /// {state} [state_id] - description
 /// /// {state} [state_id] - description
 /// /// {state} [state_id] - description
-annotations.state = {
+annotations.states = {
+  alias: [ 'state' ],
   parse() {
-    let states = this.annotation.contents.split('\n')
-    let [ markup_id = '0', state_line ] = regex('state_id', this.annotation.line)
-    states.unshift(state_line)
+    let [ markup_id = null, state_line ] = regex('state_id', this.annotation.line)
+    let state = [ state_line, ...to.array(this.annotation.contents) ].filter(Boolean)
 
-    states = states.filter(Boolean).map((line, i) => {
-      let [ state = '', state_id = `${i}`, description = '' ] = regex('state', line)
+    state = state.reduce((previous, current, i) => {
+      let [ state = '', id = `${i}`, description = '' ] = regex('state', current) // eslint-disable-line
+      return to.extend(previous, {
+        [id]: { state, description: markdown(description) }
+      })
+    }, {})
 
-      return {
-        state,
-        state_id,
-        description: markdown(description)
-      }
-    })
+    Object.defineProperty(state, '__details', { __proto__: null, value: this })
 
-    return [
-      { markup_id, states }
-    ]
+    return [ { markup_id, state } ]
   },
-  // resolve() {
-  //   /// @todo {10} - add code to that adds the markup code for this stuff.
-  // }
+  resolve({ parsed, block, log }) {
+    return this.reduce((previous, { markup_id, state }) => {
+      let markup
+      let start_at = state.__details.annotation.start
+
+      // throw an error because a state should always be accompanied by a `@markup` block
+      if (!parsed.markup) {
+        log.emit('error', "There's no instance of a '@markup' annotation")
+      } else if (is.falsy(markup_id)) {
+        markup = findMarkupAfter(parsed.markup, start_at)
+        markup_id = markup.id
+
+        if (!markup) {
+          log.emit('error', to.normalize(`
+            There's no instance of a '@markup' annotation after line ${block.comment.start + start_at}
+            in ${block.file.path}
+          `))
+        }
+      } else {
+        markup = findMarkupById(parsed.markup, markup_id)
+        if (!markup) {
+          log.emit('error', to.normalize(`
+            There's no instance of a '@markup' annotation with an id of ${markup_id}
+            in ${block.file.path}
+          `))
+        }
+      }
+
+      // return just the state and a empty markup block
+      // because a markup block wasn't found
+      if (!markup) {
+        return to.merge(previous, {
+          [markup_id]: [ { state, markup: {} } ]
+        })
+      }
+
+
+      // filter out the `raw_stateless`, and `escaped_stateless` keys because this is
+      // a state so it shouldn't have a stateless instance
+      markup = to.filter(to.clone(markup), ({ key }) => !is.in(key, 'state'))
+
+      // this allows users to specify interpolations like `@state.description`
+      // without affecting the actual state output
+      let _state = to.clone(state)
+      to.extend(_state, _state[to.keys(_state)[0]])
+      markup.raw = replaceStates(markup.raw, _state)
+      markup.escaped = replaceStates(markup.escaped, _state)
+
+      return to.merge(previous, {
+        [markup_id]: [ { state, markup } ]
+      })
+    }, {})
+
+
+    function replaceStates(str, states) {
+      // @todo {5} - Update this to use language settings
+      //  - annotation name and it's aliases
+      //  - interpolation
+      //  - annotation prefix
+      // const state_interpolation = new RegExp(`${settings.interpolation.start}${settings.annotation.prefix}etc....`, 'gi')
+      const state_interpolation = /\${@(?:state|states)[^\}]*\}/g
+      return str.replace(state_interpolation, (original_match) => {
+        let match = original_match.replace(/\${@(?:states|state)|}/g, '').slice(1)
+
+        if (!match) {
+          return states.state
+        }
+
+        let dot_index = match.indexOf('.')
+        let bracket_index = match.indexOf('[')
+        let index = dot_index > bracket_index ? dot_index : bracket_index
+
+        if (index > -1) {
+          let key = clean(match.slice(0, index))
+          let item = clean(match.slice(index))
+          return (states[key] || {})[item]
+        }
+
+        return states[clean(match)]
+      })
+    }
+
+    function clean(str) {
+      return str.replace(/[\[\]\.]/g, '')
+    }
+
+    function findMarkupById(markup, id) {
+      for (let item of markup) {
+        if (item.id === id) {
+          return item
+        }
+      }
+      return
+    }
+
+    function findMarkupAfter(markup, start_at) {
+      for (let markup_block of markup) {
+        if (start_at < markup_block.__details.annotation.start) {
+          return markup_block
+        }
+      }
+      return
+    }
+  }
 }
 
 /// @name @throws
