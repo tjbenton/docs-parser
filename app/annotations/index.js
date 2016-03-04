0c22f5d976526ef0c552fd7a8dd1edb10def06f4
Tyler Benton <tjbenton21@gmail.com>
2016-03-04 15:53:34 -0500

    removed a missed comment

--- app/annotations/index.js
+++ app/annotations/index.js
@@ -1,31 +1,3 @@
-// export default {
-//   access: require('./access.js'),
-//   alias: require('./alias.js'),
-//   arg: require('./arg.js'),
-//   author: require('./author.js'),
-//   blockinfo: require('./blockinfo.js'),
-//   chainable: require('./chainable.js'),
-//   deprecated: require('./deprecated.js'),
-//   description: require('./description.js'),
-//   markdown: require('./markdown.js'),
-//   markup: require('./markup.js'),
-//   name: require('./name.js'),
-//   note: require('./note.js'),
-//   page: require('./page.js'),
-//   'raw-code': require('./raw-code.js'),
-//   readonly: require('./readonly.js'),
-//   requires: require('./requires.js'),
-//   returns: require('./returns.js'),
-//   since: require('./since.js'),
-//   states: require('./states.js'),
-//   throws: require('./throws.js'),
-//   todo: require('./todo.js'),
-//   type: require('./type.js'),
-//   version: require('./version.js'),
-// }
-
-
-
 export access from './access'
 export alias from './alias'
 export arg from './arg'
